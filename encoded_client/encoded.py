6a8d988f49e6eb544af1a6ccb67b6adff2f0d645
Diane Trout <diane@ghic.org>
2021-11-08 10:24:51 -0800

    Add load_auth to try different methods to find credentials.
    Currently it looks for environment variables DCC_API_KEY (username)
    and DCC_SECRET_KEY (password) then if a .netrc file is present it will
    load from that
    
    Lastly if only DCC_API_KEY is set it will try loading keyring to get
    the secret key

--- encoded_client/encoded.py
+++ encoded_client/encoded.py
@@ -144,8 +144,9 @@ class ENCODED:
         self.server = server
         self.scheme = "https"
         self._session = requests.session()
-        self.username = os.environ.get("DCC_API_KEY")
-        self.password = os.environ.get("DCC_SECRET_KEY")
+        self.username = None
+        self.password = None
+        self.load_auth()
         self._user = None
         self.contexts = contexts if contexts else ENCODED_CONTEXT
         self.namespaces = namespaces if namespaces else ENCODED_NAMESPACES
@@ -160,6 +161,22 @@ class ENCODED:
             return None
 
         return (self.username, self.password)
+
+    def load_auth(self):
+        if self.username is None:
+            self.username = os.environ.get("DCC_API_KEY")
+            self.password = os.environ.get("DCC_SECRET_KEY")
+
+        if self.username is None:
+            self.load_netrc()
+
+        if self.username is not None and self.password is None:
+            try:
+                import keyring
+                self.password = keyring.get_password(self.server, self.username)
+            except ImportError:
+                LOGGER.info("Unable to load password via keyring")
+                pass
 
     def load_netrc(self):
         netrc_path = Path("~/.netrc").expanduser()
